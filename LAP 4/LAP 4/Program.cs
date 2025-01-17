f416e0b07e6e0f46ebc6d556c53c0a64a6fe8640
huynchanie <nguyenthihuyentrang03062004@gmail.com>
2025-01-17 10:04:11 +0700

    Buổi 3

---
+++ LAP 4/LAP 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAP_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lap3/Lap3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           Student student = new Student();
+            student.DisplayInfo();
+           Student student2 = new Student("Trang", "Huyen", new DateTime(2004, 06, 03));
+            student2.DisplayInfo();
+
+        }
+    }
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DoB { get; set; }
+        public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+        private static int totalStudents;
+
+
+        public Student( ) 
+        {
+            FirstName = "Default FirstName";
+            LastName = "Default Lastname";
+            DoB = null;
+            totalStudents++;
+        
+        }
+        public Student(string firstName, string lastName, DateTime? dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DoB = dateOfBirth ?? new DateTime(2006, 1, 1);
+            totalStudents++;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {StudentID}, Name: {FirstName} {LastName}, Date of Birth: {DoB}, Enrollment Date: {EnrollmentDate}");
+        }
+    }
+
+}
